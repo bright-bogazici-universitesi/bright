95dcc91f7f208675c323837cee77f7dd667a2a1a
Samed Duzcay <samedduzcay@gmail.com>
2017-01-31 23:13:37 +0300

    fix package.json, minor changes

--- app/resources/views/layouts/base.blade.php
+++ app/resources/views/layouts/base.blade.php
@@ -104,7 +104,7 @@
                             </a>
                         </li>
                     </ul>
-                    <p class="copyright text-muted">Copyright &copy; Your Website 2016</p>
+                    <p class="copyright text-muted">Copyright &copy; Bright 2017</p>
                 </div>
             </div>
         </div>
