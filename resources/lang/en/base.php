cf0913808d2e75e09dcfefd2db26545b6522e3d8
Samed Duzcay <samedduzcay@gmail.com>
2017-02-10 21:07:26 +0300

    Remove compiled files from git, finish navbar

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -2,11 +2,12 @@
 
 return [
   // Navbar texts
-  'home'           => 'Home',
-  'programs'       => 'Programs',
-  'events'         => 'Events',
-  'knowledge_base' => 'Knowledge Base',
-  'contact'        => 'Contact',
-  'blog'           => 'Blog',
-  'subscribe'      => 'Subscribe'
+  'home'              => 'Home',
+  'programs'          => 'Programs',
+  'events'            => 'Events',
+  'knowledge_base'    => 'Knowledge Base',
+  'contact'           => 'Contact',
+  'blog'              => 'Blog',
+  'subscribe'         => 'Subscribe',
+  'shopping_bag_icon' => 'Shopping bag icon'
 ];
--- resources/lang/tr/base.php
+++ resources/lang/tr/base.php
@@ -2,11 +2,12 @@
 
 return [
   // Navbar texts
-  'home'           => 'Anasayfa',
-  'programs'       => 'Programlar',
-  'events'         => 'Etkinlikler',
-  'knowledge_base' => 'Bilgi Bankası',
-  'contact'        => 'İletişim',
-  'blog'           => 'Blog',
-  'subscribe'      => 'Abone Ol'
+  'home'              => 'Anasayfa',
+  'programs'          => 'Programlar',
+  'events'            => 'Etkinlikler',
+  'knowledge_base'    => 'Bilgi Bankası',
+  'contact'           => 'İletişim',
+  'blog'              => 'Blog',
+  'subscribe'         => 'Abone Ol',
+  'shopping_bag_icon' => 'Shopping bag icon'
 ];
