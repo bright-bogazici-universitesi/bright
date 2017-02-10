78eca19ef7d7e5cefd4e0c9949d93447c667ae1e
Samed Duzcay <samedduzcay@gmail.com>
2017-02-10 21:23:41 +0300

    Add lang file from homepage

---
+++ resources/lang/en/home.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'hero_heading'    => 'Girişimciler yetiştiriyor, girişimlere yol gösteriyoruz.',
+  'hero_subheading' => 'Bilgi birikimimizi ve kültürümüzü ekosistemle buluşturuyoruz.'
+];
---
+++ resources/lang/tr/home.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'hero_heading'    => 'We train entrepreneurs and guide the startups.',
+  'hero_subheading' => 'We connect our knowledge and culture with the ecosystem.'
+];
