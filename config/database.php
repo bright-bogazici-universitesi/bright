bc16bfeb5b922b7f750a23d45cf49c5ebdbe291b
Samed Duzcay <samedduzcay@gmail.com>
2017-02-17 16:58:33 +0300

    Change getenv to env, for some reason

--- config/database.php
+++ config/database.php
@@ -68,10 +68,10 @@ return [
 
         'heroku' => [
             'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'     => parse_url(env("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(env("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(env("DATABASE_URL"))["user"],
+            'password' => parse_url(env("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
