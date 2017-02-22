c4d76f4eda9b0f36ec0fd7ae1c1db179f7311295
Samed Duzcay <samedduzcay@gmail.com>
2017-02-22 17:32:54 +0300

    Revert mistake from last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,10 @@
                   <span class="subheading">{{ __('home.hero_subheading') }}</span>
               </div>
           </div>
+          <a class="subscribe-button cta-button"
+             href="/{{ $locale }}/programs">
+             Our Freshest Program
+          </a>
           <a class="bouncing-down-arrow"
              href="#section-1">
             <i class="fa fa-2x fa-angle-down"></i>
