d5848f34bbb392dccb8feb36fb4b1e3f1229e3ba
Samed Duzcay <samedduzcay@gmail.com>
2017-02-22 19:21:38 +0300

    Add cta buttons to home page sections

--- resources/views/partials/home/section-4.blade.php
+++ resources/views/partials/home/section-4.blade.php
@@ -14,22 +14,31 @@
 
 @section('extras')
 
-  @foreach($items = [
-    ["name" => "Yönetim Kurulu", "photo" => "user"],
-    ["name" => "Yönetim Kurulu", "photo" => "user"],
-    ["name" => "Yönetim Kurulu", "photo" => "user"],
-    ["name" => "Yönetim Kurulu", "photo" => "user"]
-  ] as $item)
+  <div class="row">
+    @foreach($items = [
+      ["name" => "Yönetim Kurulu", "photo" => "user"],
+      ["name" => "Yönetim Kurulu", "photo" => "user"],
+      ["name" => "Yönetim Kurulu", "photo" => "user"],
+      ["name" => "Yönetim Kurulu", "photo" => "user"]
+    ] as $item)
 
-    <div class="section-4-img-block text-center col-md-3 col-xs-6">
-      <div>
-        <h3 style="margin-top: 0">{{ $item['name'] }}</h3>
+      <div class="section-4-img-block text-center col-md-3 col-xs-6">
+        <div>
+          <h3 style="margin-top: 0">{{ $item['name'] }}</h3>
+        </div>
+        <div class="icon">
+          <i class="fa fa-4x fa-{{ $item['photo'] }}"></i>
+        </div>
       </div>
-      <div class="icon">
-        <i class="fa fa-4x fa-{{ $item['photo'] }}"></i>
-      </div>
-    </div>
 
-  @endforeach
+    @endforeach
+  </div>
+
+  <div class="row text-center margin-top-50">
+    <a href="/{{ $locale }}/programs"
+       class="btn btn-default cta-button cta-button-blue">
+         {{ __('home.meet_board') }}
+       </a>
+  </div>
 
 @overwrite
